e07916d50b06ec68bb1f95f9f9df272c251eb359
Asafe Matheus <asafesousa66@outlook.com>
2021-04-20 12:17:14 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Text, View, StyleSheet} from 'react-native'
+import Wellcome from './src/pages/Wellcome'
+
+export default function App(){
+  return (
+    <Wellcome />
+  )
+}
